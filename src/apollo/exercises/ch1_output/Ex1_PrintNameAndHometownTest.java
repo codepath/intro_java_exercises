56b4db2ea282d2edf8979a2ce9eb6ca2934876e8
Timothy Lee <timothy1ee@yahoo.com>
2012-12-20 15:06:36 -0800

    Added basic chapter 1 (Output) exercise classes.

---
+++ src/apollo/exercises/ch1_output/Ex1_PrintNameAndHometownTest.java
@@ -0,0 +1,16 @@
+package apollo.exercises.ch1_output;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import apollo.exercises.test.TestBase;
+
+public class Ex1_PrintNameAndHometownTest extends TestBase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
