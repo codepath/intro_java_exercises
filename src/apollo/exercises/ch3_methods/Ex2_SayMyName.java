2f7258c4c2ab6ebfe545c30eeded1d6d0b55f24c
Timothy Lee <timothy1ee@yahoo.com>
2012-12-26 20:34:13 -0600

    [Chapter 3 - Methods] Adds a couple of basic exercises.

---
+++ src/apollo/exercises/ch3_methods/Ex2_SayMyName.java
@@ -0,0 +1,26 @@
+package apollo.exercises.ch3_methods;
+
+// This is a program that's really good at saying names.
+public class Ex2_SayMyName {
+
+	// This is the main method that is executed as
+	// soon as the program starts.
+	public static void main(String[] args) {
+
+		String name1 = "Billy";
+		String name2 = "Jake";
+		String name3 = "Michael";
+		
+		// Create a public static method called SayMyName
+		// that takes a single String parameter. SayMyName
+		// should print a customized message based on the
+		// parameter.
+		//
+		// Call SayMyName three times in the main function
+		// for the three names.
+		//
+		// Example output:
+		//  Hello Billy, it's good to meet you!
+	}
+
+}
