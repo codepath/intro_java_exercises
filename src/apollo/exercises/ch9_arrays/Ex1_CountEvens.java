1abab7a11fb26f075b4cd7d89a2ee3bb08959f46
Timothy Lee <timothy1ee@yahoo.com>
2012-12-27 03:27:15 -0600

    [Classes, Arrays] Add basic exercises.

---
+++ src/apollo/exercises/ch9_arrays/Ex1_CountEvens.java
@@ -0,0 +1,19 @@
+package apollo.exercises.ch9_arrays;
+
+public class Ex1_CountEvens {
+
+	// This is the main method that is executed as
+	// soon as the program starts.	
+	public static void main(String[] args) {
+
+		// Create a method called countEvens
+		// Return the number of even ints in the given array. 
+		// Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1. 
+
+		// countEvens({2, 1, 2, 3, 4}) -> 3
+		// countEvens({2, 2, 0}) -> 3
+		// countEvens({1, 3, 5}) -> 0
+		
+	}
+
+}
