2a8a01d87727eedda0acce22ebda77564279bf0c
Timothy Lee <timothy1ee@yahoo.com>
2012-12-27 00:46:22 -0600

    [Chapter 6 - More loops] Adds a basic exercise.

---
+++ src/apollo/exercises/ch6_moreLoops/Ex1_GreatestCommonFactor.java
@@ -0,0 +1,19 @@
+package apollo.exercises.ch6_moreLoops;
+
+public class Ex1_GreatestCommonFactor {
+
+	// This is the main method that is executed as
+	// soon as the program starts.	
+	public static void main(String[] args) {
+		
+		// Create a method called greatestCommonFactor
+		// It should return the greatest common factor
+		// of a number.  One way to find the greatest
+		// common factor is just to keep trying larger
+		// and larger numbers.
+		//
+		// Consider using a while loop.
+		
+	}
+
+}
