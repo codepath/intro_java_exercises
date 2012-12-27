f3f87c86ed7fd67c3cbf565b1bdf40ef4279bb0f
Timothy Lee <timothy1ee@yahoo.com>
2012-12-27 00:23:22 -0600

    [Chapter 5 - Conditionals] Adds basic exercises.

---
+++ src/apollo/exercises/ch5_conditionals/Ex2_RomanNumerals.java
@@ -0,0 +1,19 @@
+package apollo.exercises.ch5_conditionals;
+
+public class Ex2_RomanNumerals {
+
+	// This is the main method that is executed as
+	// soon as the program starts.	
+	public static void main(String[] args) {
+		
+		// Create a method that takes a number between 1-10
+		// and prints the equivalent roman numeral.
+		//
+		// You should check to make sure the number passed
+		// in isn't greater than 10.  If it is, print an
+		// error.
+		//
+		
+	}
+
+}
