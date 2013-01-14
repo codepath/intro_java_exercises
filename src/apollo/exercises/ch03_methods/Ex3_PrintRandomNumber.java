39cdf32d55f358067a0f303a3e974e5307a81cdf
Nathan Esquenazi <nesquena@gmail.com>
2013-01-14 15:13:24 -0800

    Adds hint about how to generate randomly

--- src/apollo/exercises/ch03_methods/Ex3_PrintRandomNumber.java
+++ src/apollo/exercises/ch03_methods/Ex3_PrintRandomNumber.java
@@ -2,18 +2,22 @@ package apollo.exercises.ch03_methods;
 
 public class Ex3_PrintRandomNumber {
 
-	// This is the main method that is executed as
-	// soon as the program starts.
-	public static void main(String[] args) {
-		
-		// Create a method that returns a random number
-		// between min and max.
-		//
-		// PrintRandomNumber(int min, int max)
-		//
-		// Save the return value in a variable and print
-		// the random number.
-		
-	}
+  // This is the main method that is executed as
+  // soon as the program starts.
+  public static void main(String[] args) {
+
+    // Create a method that returns a random number
+    // between min and max.
+    //
+    // PrintRandomNumber(int min, int max)
+    //
+    // Save the return value in a variable and print
+    // the random number.
+    //
+    // HINT: To generate a random number from 0 to 40:
+    //
+    //    Random rand = new Random();
+    //    rand.nextInt(40);
+  }
 
 }
