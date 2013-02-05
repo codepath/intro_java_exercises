f64410a4921aa971673b05304598bc89365f9144
Timothy Lee <timothy1ee@yahoo.com>
2013-02-04 19:57:28 -0800

    Update Chapter 6 exercises.

--- src/apollo/exercises/ch06_classes/Ex1_BookRunner.java
+++ src/apollo/exercises/ch06_classes/Ex1_BookRunner.java
@@ -6,8 +6,6 @@ public class Ex1_BookRunner {
 	// soon as the program starts.	
 	public static void main(String[] args) {
 
-		// Book textBook = new Book();
-		// System.out.println(textBook);  //What does this line do?
 	}
 
 }
